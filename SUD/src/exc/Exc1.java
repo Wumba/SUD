444bfd4679b09edb9729d90686c05904e6f9772c
wuma <j.antonius@gmx.net>
2020-09-28 08:49:16 +0200

    make inputReader class for reusing

--- SUD/src/exc/Exc1.java
+++ SUD/src/exc/Exc1.java
@@ -15,134 +15,15 @@ public class Exc1 {
     private final LinkedHashMap<Integer, Double> orderMap;
     private double sum;
     BufferedReader bufferedReader;
+    InputReader inputReader;
 
     public Exc1() {
         receiveInput = true;
         orderMap = new LinkedHashMap<>();
         sum = 0;
+        inputReader = new InputReader();
     }
 
-    /**
-     * Read console and return input as string
-     *
-     * @return String
-     */
-    private String readDataFromConsole() {
-        String output;
-        this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        // todo: why does autoclosable not work? Why can't I close it at all?
-        try {
-            output = bufferedReader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-            LOG.log(Level.SEVERE, "Something went wrong");
-            output = "Error";
-        }
-        return output;
-    }
-
-    private void closeReader() {
-        try {
-            bufferedReader.close();
-        } catch (IOException exception) {
-            LOG.log(Level.SEVERE, "Reader can't be closed!");
-        }
-    }
-
-    /**
-     * Receives user input and tries find an Double in input.
-     * Finding is parsed into Double.
-     *
-     * @param input String
-     * @return Double
-     */
-    private double checkAndParsePriceInput(String input) {
-        Pattern doublePoint = Pattern.compile("\\d+\\.\\d+");
-        Pattern doubleComma = Pattern.compile("\\d+\\,\\d+");
-        Pattern withoutCommaAndDot = Pattern.compile("\\d+");
-        Matcher matcherPoint = doublePoint.matcher(input);
-        Matcher matcherComma = doubleComma.matcher(input);
-        Matcher withoutCommaAndDotMatcher = withoutCommaAndDot.matcher(input);
-
-        if (input.isEmpty()) {
-            LOG.log(Level.SEVERE, "Please enter at least something!");
-            return 0.0;
-        } else {
-            if (matcherPoint.find()) {
-                String priceAsString = matcherPoint.group(0);
-                return parseStringToDouble(priceAsString);
-
-            } else if (matcherComma.find()) {
-                String priceAsString = matcherComma.group(0);
-                String processed = priceAsString.replace(",", ".");
-                return parseStringToDouble(processed);
-
-            } else if (withoutCommaAndDotMatcher.find()) {
-                String priceAsString = withoutCommaAndDotMatcher.group(0);
-                return parseStringToDouble(priceAsString);
-            } else {
-                LOG.log(Level.SEVERE, "Please enter a number.");
-                return 0.0;
-            }
-        }
-    }
-
-    /**
-     * Receives user input and tries find an Integer in input.
-     * Finding is parsed into Integer.
-     *
-     * @param input String
-     * @return Integer
-     */
-    private int checkAndParseAmountInput(String input) {
-        if (input.isEmpty()) {
-            LOG.log(Level.SEVERE, "Please enter at least something!");
-            return 0;
-        } else {
-            Pattern amountPattern = Pattern.compile("\\d+");
-            Matcher amountMatcher = amountPattern.matcher(input);
-
-            if (amountMatcher.find()) {
-                String amountAsString = amountMatcher.group(0);
-                return parseStringToInt(amountAsString);
-            } else {
-                LOG.log(Level.SEVERE, "Please enter a number.");
-                return 0;
-            }
-        }
-    }
-
-    /**
-     * Parse String to Double
-     *
-     * @param input String
-     * @return Double
-     */
-    private double parseStringToDouble(String input) {
-        try {
-            return Double.parseDouble(input);
-        } catch (NullPointerException | NumberFormatException exception) {
-            LOG.log(Level.SEVERE, "The format is wrong. Try to enter another price.");
-            exception.printStackTrace();
-            return 0.0;
-        }
-    }
-
-    /**
-     * Parse String to Integer
-     *
-     * @param input String
-     * @return Integer
-     */
-    private int parseStringToInt(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NullPointerException | NumberFormatException exception) {
-            LOG.log(Level.SEVERE, "The format is wrong or empty. Try to enter another amount.");
-            exception.printStackTrace();
-            return 0;
-        }
-    }
 
     /**
      * Save parsed user inputs to map
@@ -205,23 +86,23 @@ public class Exc1 {
 
             while(intData == 0) {
                 System.out.println("Please enter the amount of items:");
-                String data = readDataFromConsole();
-                intData = checkAndParseAmountInput(data);
+                String data = inputReader.readDataFromConsoleToString();
+                intData = inputReader.checkAndParseIntegerInput(data);
             }
 
             while(doubleData == 0) {
                 System.out.println("Please enter the belonging price for a single item.");
-                String data2 = readDataFromConsole();
-                doubleData = checkAndParsePriceInput(data2);
+                String data2 = inputReader.readDataFromConsoleToString();
+                doubleData = inputReader.checkAndParseDoubleInput(data2);
             }
             writeToMap(intData, doubleData);
 
             System.out.println("Would you like to enter another article? [y/n]");
-            String data3 = readDataFromConsole();
+            String data3 = inputReader.readDataFromConsoleToString();
             exitInput(data3);
         }
         printTheMap();
-        closeReader();
+        inputReader.closeReader();
     }
 
     public static void main(String[] args) {
---
+++ SUD/src/exc/InputReader.java
@@ -0,0 +1,147 @@
+package exc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputReader {
+    private static final Logger LOG = Logger.getLogger(Exc1.class.getName());
+    private boolean receiveInput;
+    private final LinkedHashMap<Integer, Double> orderMap;
+    private double sum;
+    BufferedReader bufferedReader;
+
+    public InputReader() {
+        receiveInput = true;
+        orderMap = new LinkedHashMap<>();
+        sum = 0;
+    }
+
+    /**
+     * Read console and return input as string
+     *
+     * @return String
+     */
+    public String readDataFromConsoleToString() {
+        String output;
+        this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // todo: why does autoclosable not work? Why can't I close it at all?
+        try {
+            output = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOG.log(Level.SEVERE, "Something went wrong");
+            output = "Error";
+        }
+        return output;
+    }
+
+    public void closeReader() {
+        try {
+            bufferedReader.close();
+        } catch (IOException exception) {
+            LOG.log(Level.SEVERE, "Reader can't be closed!");
+        }
+    }
+
+    /**
+     * Receives user input and tries find an Double in input.
+     * Finding is parsed into Double.
+     *
+     * @param input String
+     * @return Double
+     */
+    public double checkAndParseDoubleInput(String input) {
+        Pattern doublePoint = Pattern.compile("\\d+\\.\\d+");
+        Pattern doubleComma = Pattern.compile("\\d+\\,\\d+");
+        Pattern withoutCommaAndDot = Pattern.compile("\\d+");
+        Matcher matcherPoint = doublePoint.matcher(input);
+        Matcher matcherComma = doubleComma.matcher(input);
+        Matcher withoutCommaAndDotMatcher = withoutCommaAndDot.matcher(input);
+
+        if (input.isEmpty()) {
+            LOG.log(Level.SEVERE, "Please enter at least something!");
+            return 0.0;
+        } else {
+            if (matcherPoint.find()) {
+                String doubleAsString = matcherPoint.group(0);
+                return parseStringToDouble(doubleAsString);
+
+            } else if (matcherComma.find()) {
+                String doubleAsString = matcherComma.group(0);
+                String processed = doubleAsString.replace(",", ".");
+                return parseStringToDouble(processed);
+
+            } else if (withoutCommaAndDotMatcher.find()) {
+                String doubleAsString = withoutCommaAndDotMatcher.group(0);
+                return parseStringToDouble(doubleAsString);
+            } else {
+                LOG.log(Level.SEVERE, "Please enter a number.");
+                return 0.0;
+            }
+        }
+    }
+
+    /**
+     * Receives user input and tries find an Integer in input.
+     * Finding is parsed into Integer.
+     *
+     * @param input String
+     * @return Integer
+     */
+    public int checkAndParseIntegerInput(String input) {
+        if (input.isEmpty()) {
+            LOG.log(Level.SEVERE, "Please enter at least something!");
+            return 0;
+        } else {
+            Pattern intPattern = Pattern.compile("\\d+");
+            Matcher intMatcher = intPattern.matcher(input);
+
+            if (intMatcher.find()) {
+                String amountAsString = intMatcher.group(0);
+                return parseStringToInt(amountAsString);
+            } else {
+                LOG.log(Level.SEVERE, "Please enter a number.");
+                return 0;
+            }
+        }
+    }
+
+    /**
+     * Parse String to Double
+     *
+     * @param input String
+     * @return Double
+     */
+    private double parseStringToDouble(String input) {
+        try {
+            return Double.parseDouble(input);
+        } catch (NullPointerException | NumberFormatException exception) {
+            LOG.log(Level.SEVERE, "The format is wrong. Try to enter another price.");
+            exception.printStackTrace();
+            return 0.0;
+        }
+    }
+
+    /**
+     * Parse String to Integer
+     *
+     * @param input String
+     * @return Integer
+     */
+    private int parseStringToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NullPointerException | NumberFormatException exception) {
+            LOG.log(Level.SEVERE, "The format is wrong or empty. Try to enter another amount.");
+            exception.printStackTrace();
+            return 0;
+        }
+    }
+}
+
