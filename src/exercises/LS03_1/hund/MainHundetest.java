03ef964b7c2959c90a459a69a3bae1f1661a1b20
Wumba <compl3xPSWDsuperDuper>
2020-11-15 16:37:41 +0100

    add Hund

---
+++ src/exercises/LS03_1/hund/MainHundetest.java
@@ -0,0 +1,12 @@
+package exercises.LS03_1.hund;
+public class MainHundetest {
+
+	public static void main(String[] args) {
+		Hund bob = new Hund();
+		bob.setGewicht(40.8);
+		bob.setGroesse(80);
+		bob.setRasse("Collie");
+		bob.ausgabe();
+	}
+
+}
