fd33a000b7299c36ea0e89a20a79872cb7e8dccd
Wumba <j.antonius@gmx.net>
2021-01-16 17:04:43 +0100

    Finish Circle.java

--- src/exercises/LS03_3/Circle.java
+++ src/exercises/LS03_3/Circle.java
@@ -3,6 +3,18 @@ package exercises.LS03_3;
 class Circle {
     private double radius;
 
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if (radius > 0) {
+            this.radius = radius;
+        } else {
+            throw new IllegalArgumentException("Radius must be above 0");
+        }
+    }
+
     public Circle(double radius) {
         this.radius = radius;
     }
