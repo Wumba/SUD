9d8663fa0d08da7f65b93308de955fa28705901e
Wumba <compl3xPSWDsuperDuper>
2021-01-25 08:56:08 +0100

    finish NumberSystemConversion

--- src/exercises/LS03_3/numberConversion/NumberSystemConversion.java
+++ src/exercises/LS03_3/numberConversion/NumberSystemConversion.java
@@ -7,24 +7,17 @@ public class NumberSystemConversion {
     UserInteractionInputHandler userInteractionInputHandler = new UserInteractionInputHandler();
 
 
-    private ENumberSystems chooseBase() {
-       ENumberSystems startBase = null;
+    private ENumberSystems chooseBase(String text) {
+        ENumberSystems startBase = null;
         while (startBase == null) {
-            System.out.println("Please enter a number system [binary, octa, deci, hex]");
+            System.out.println(text);
             String input = userInteractionInputHandler.readDataFromConsoleToString();
             switch (input) {
-                case "binary", "2" -> {
-                    startBase = ENumberSystems.BINARY;
-                }
-                case "octa", "8" -> {
-                    startBase =  ENumberSystems.OCTA;
-                }
-                case "deci", "10" -> {
-                    startBase =  ENumberSystems.DECI;
-                }
-                case "hex", "16" -> {
-                    startBase =  ENumberSystems.HEXA;
-                }
+                case "binary", "2" -> startBase = ENumberSystems.BINARY;
+                case "octa", "8" -> startBase = ENumberSystems.OCTA;
+                case "deci", "10" -> startBase = ENumberSystems.DECI;
+                case "hex", "16" -> startBase = ENumberSystems.HEXA;
+                case "" -> System.out.println("Please enter something!");
                 default -> System.out.println("This number system does not exist. Please try again.");
             }
         }
@@ -33,16 +26,47 @@ public class NumberSystemConversion {
 
     public static String baseConversion(String number,
                                         ENumberSystems previousBase, ENumberSystems newBase) {
-        int fromString = Integer.parseInt(number, previousBase.toInt());
-        return Integer.toString(fromString, newBase.toInt());
-    }
+        boolean repeat = true;
+        int parsedInt = 0;
+        while (repeat) {
+            try {
+                parsedInt = Integer.parseInt(number, previousBase.toInt());
+                repeat = false;
+            } catch (NumberFormatException e) {
+                System.out.println("The number system and the number doesn't match. Please try again");
+            }
+        }
 
+        String convertedString = "";
+        repeat = true;
+        while (repeat) {
+            try {
+                convertedString = Integer.toString(parsedInt, newBase.toInt());
+                repeat = false;
+            } catch (NumberFormatException e) {
+                System.out.println("The number could not converted. Please try again.");
+            }
+        }
+
+        return convertedString;
+    }
 
 
     public void speakWithUser() {
         System.out.println("please enter a number of one of the following number systems: hex, octa, deci or binary");
         String inputNumber = userInteractionInputHandler.readDataFromConsoleToString();
-
+        ENumberSystems inputNumberBase = chooseBase("Please enter a number system [binary, octa, deci, hex]");
+        ENumberSystems goalNumberSystem = chooseBase("Please enter the wished number system you want to convert in [binary, octa, deci, hex].");
+        String convertedNumber = baseConversion(inputNumber, inputNumberBase, goalNumberSystem);
+        System.out.println("number: "
+                + inputNumber
+                + "; system: "
+                + inputNumberBase
+                + " -> "
+                + "converted number: "
+                + convertedNumber
+                + "; system: "
+                + goalNumberSystem);
     }
 }
 
