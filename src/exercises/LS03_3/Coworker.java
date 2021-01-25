3c9931713344ea32bbdd774d9947ef0306b11bc2
Wumba <compl3xPSWDsuperDuper>
2021-01-25 08:18:56 +0100

    add UUID

--- src/exercises/LS03_3/Coworker.java
+++ src/exercises/LS03_3/Coworker.java
@@ -1,15 +1,21 @@
 package exercises.LS03_3;
 
+import java.util.UUID;
+
 public class Coworker {
-    private int id;
+    private UUID id;
     private String name;
 
-    public int getId() {
+    public UUID getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(UUID id) {
         this.id = id;
+    }
+
+    public void generateNewUUID() {
+        this.id = UUID.randomUUID();
     }
 
     public String getName() {
